d61f2b213aa0fcc146e3ff0e7452a591b3034576
Leonardo Malta <55796961+leonardodma@users.noreply.github.com>
2019-11-23 11:58:52 -0300

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -1,1 +1,19 @@
+import pygame
+import random
+from os import path
 
+
+font_name = pygame.font.match_font('arial')
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+WIDTH = 1150
+HEIGHT = 654
+FPS = 60
+
+# Estabelece a pasta que contem as figuras.
+img_dir = path.join(path.dirname(__file__), 'game_files')
+# Estabelece a pasta que contem os sons.
+snd_dir = path.join(path.dirname(__file__), 'game_files')
