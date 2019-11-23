7fc0b10e1edc024587d861532d8d640f61ef84ac
Leonardo Malta <55796961+leonardodma@users.noreply.github.com>
2019-11-23 11:59:49 -0300

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -25,3 +25,36 @@ def draw_text(surf, text, size, x, y):
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
+    
+
+    class Players(pygame.sprite.Sprite):
+    def __init__(self, posicao):
+        pygame.sprite.Sprite.__init__(self)
+        player1_img = pygame.image.load(path.join(img_dir, 'luva_player_1.png'))  # Define imagem do Player1
+        player2_img = pygame.image.load(path.join(img_dir, 'luva_player_2.png'))  # Define imagem do Player2
+
+        self.image = pygame.transform.scale(player1_img, (80, 80))  # Define Proporções
+        self.image.set_colorkey(BLACK)  # Deixa Transparente
+
+        self.rect = self.image.get_rect()  # Obtem detalhes da posição
+
+        if posicao == "esquerda":
+            self.image = player1_img
+            self.image = pygame.transform.scale(player1_img, (80, 80))  # Define Proporções
+            self.rect.centerx = WIDTH - 980  # Posiciona no centro em x
+
+        elif posicao == "direita":
+            self.image = player2_img
+            self.image = pygame.transform.scale(player2_img, (80, 80))  # Define Proporções
+            self.rect.centerx = WIDTH - 175  # Posiciona no centro em x
+
+        self.rect.bottom = HEIGHT / 2  # Posiciona objeto na parte inferior em y
+
+        self.speedy = 0
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
